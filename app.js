548df72a733dc67e75e4b85493afe28d959026bb
SergioM94 <64223283+SergioM94@users.noreply.github.com>
2022-01-16 00:21:02 +0100

    added random sentence generator

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ let currentDate = new Date ($.now())
 let currentYear = currentDate.getFullYear();
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 let monthName = month[currentDate.getMonth()];
-let currentDay = currentDate.getUTCDate() 
+let currentDay = currentDate.getUTCDate() //Uses UTC (not GMT+1)
 let sessionDate = $("<p>");
 sessionDate.text(`--${monthName} ${currentDay}, ${currentYear}--`);
-console.log(sessionDate);
 $("#session-date").append(sessionDate);
 
 //Show welcome message from the computer (pending delay and time)
@@ -17,6 +16,31 @@ welcomeMsg.html("<strong>Homer:</strong> Tell me, O Muse, of the man of many dev
 setTimeout(function() { 
     $("#welcome-message").append(welcomeMsg);
     }, 2000);
+
+
+//Generate random sentence (https://stackoverflow.com/questions/33160766/generate-random-sentences-from-an-array-javascript)
+let sentences= [
+    'So, surrender to sleep at last. What a misery, keeping watch through the night, wide awake -- you’ll soon come up from under all your troubles.',
+    'Nobody -- that’s my name. Nobody -- so my mother and father call me, all my friends.',
+    'Her gifts were mixed with good and evil both.',
+    'Man is the vainest of all creatures that have their being upon earth.',
+    'There is a time for making speeches, and a time for going to bed.',
+    'For there is nothing better in this world than that man and wife should be of one mind in a house.',
+    'Quick, dear boy, come in, let me look at you, look to my heart’s content -- under my own roof, the rover home at last.',
+    'But I will gladly advise him -- I’ll hide nothing--',
+    'I swear by the greatest, grimmest oath that binds the happy gods.',
+    'My every impulse bends to what is right. Not iron, trust me, the heart with my breast. I am all compassion.',
+],
+
+maxSentences = sentences.length;
+
+function generateRandomSentence () {
+    let index = Math.floor(Math.random() * (maxSentences - 1));
+    return sentences[index];
+    
+}
+
+console.log(generateRandomSentence());
 
 //Actions on submit
 $("form").on("submit",function (event) {
@@ -31,7 +55,7 @@ $("form").on("submit",function (event) {
     //Get timestamp and parse hours and minutes
     let timeStamp = new Date($.now());
     let minutes = (timeStamp.getMinutes()<10?'0':'') + timeStamp.getMinutes();
-    let time = timeStamp.getHours() + ":" + minutes + "h";
+    let time = timeStamp.getHours() + ":" + minutes + "h"; //Uses GMT+1
     
     //Append new paragraph to the message log div containing username (pending), message and time.
     let userText = `<strong>Muse:</strong> ${userInput} || ${time}`;
@@ -43,7 +67,8 @@ $("form").on("submit",function (event) {
     $(this).trigger("reset");
 
     //Display random message from the computer
-    let computerText= `<strong>Homer:</strong> Nice to see you again, my beloved friend || ${time}`;
+    let computerRandomText = generateRandomSentence();
+    let computerText= `<strong>Homer:</strong> ${computerRandomText} || ${time}`;
     let newComputerMsg = $("<div>")
         .addClass("computer-message")
         .html(computerText);
