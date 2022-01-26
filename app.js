39685ddd7ee9278abf68e06aecf31961fb3b36da
SergioM94 <64223283+SergioM94@users.noreply.github.com>
2022-01-26 19:47:21 +0100

    refactores js functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-
 let helperFunctions = {
+
     getCurrentTime() {
         let timestamp = new Date();
         let minutes = (timestamp.getMinutes()<10?'0':'') + timestamp.getMinutes(); // Attribution: https://stackoverflow.com/questions/8935414/getminutes-0-9-how-to-display-two-digit-numbers
@@ -8,28 +8,29 @@ let helperFunctions = {
     },
 
     getCurrentDate() {
-        let currentDate = new Date()
+        let currentDate = new Date();
         let currentYear = currentDate.getFullYear();
         const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
         let monthName = month[currentDate.getMonth()];
-        let currentDay = currentDate.getDate()
+        let currentDay = currentDate.getDate();
         return `${monthName} ${currentDay}, ${currentYear}`; 
-    }
-}
+    },
 
-function scrollToBottom() { //Attribution: https://stackoverflow.com/questions/2346011/how-do-i-scroll-to-an-element-within-an-overflowed-div
-    $("#message-log").scrollTop($("#message-log").scrollTop() + $("#end-of-messages-wrapper").position().top);
+    setLocalStorage (classContent, messageContent) {
+        let date = new Date();
+        localStorage.setItem(`message-${date.getTime()}`, 
+            JSON.stringify({class: classContent, message: `${messageContent}`, timestamp: `${helperFunctions.getCurrentTime()}`}));
+    },
+
+    //Attribution: https://stackoverflow.com/questions/2346011/how-do-i-scroll-to-an-element-within-an-overflowed-div
+    scrollToBottom() { 
+        $("#message-log").scrollTop($("#message-log").scrollTop() + $("#end-of-messages-wrapper").position().top);
+    },
 };
 
-$("form #user-input").focus(function () { //Pending refactoring
-    setTimeout( function(){
-    // window.scrollTo(0, 0)
-    // document.body.scrollTop = 0
-    scrollToBottom();
-    }
-, 500)});
-
 function initializeChat() {
+
+    let localStorageArr = [];
 
     function displaySessionDate() {
         let sessionDate = $("<div>");
@@ -41,19 +42,23 @@ function initializeChat() {
   
     function displayChatHistory() { //Attribution: https://www.youtube.com/watch?v=LfeOLVGHiXI&t=607s
 
-        let localStorageArr = [];
+        // In case localStorage needs to be used for other purposes, only the keys with the "message-" prefix get pushed to the array.
         for(let i=0;i<localStorage.length;i++) {
-            localStorageArr.push(localStorage.key(i)); //Usar prefijo al guardar Local storage y solo recuperar los mensajes
-        }
+            
+            if(localStorage.key(i).startsWith("message-")) {
+                localStorageArr.push(localStorage.key(i)); 
+            };
+        };
         
         let orderedLocalStorageArr = localStorageArr.sort();
 
-        for(let i=0;i<localStorage.length;i++) { //Usar length del array y no de LS
+        for(let i=0;i<localStorageArr.length;i++) { 
             
             const lsKey = orderedLocalStorageArr[i];
             const lsValue = JSON.parse(localStorage.getItem(lsKey));
             let lsMessage = $("<div>");
             let lsTimestamp = $("<div>");
+            
             if (lsValue.class === "ls-user") {
                 lsMessage
                     .html(lsValue.message)
@@ -61,10 +66,11 @@ function initializeChat() {
                 lsTimestamp
                     .html(lsValue.timestamp)
                     .addClass("user-timestamp")
-                setTimeout(function() { //Valorar eliminar timeout
+                setTimeout(function() { 
                     $("#end-of-messages-wrapper").before($(lsMessage));
                     $("#end-of-messages-wrapper").before($(lsTimestamp));
                 }, 1000)
+            
             } if (lsValue.class === "ls-computer") {
                 lsMessage
                     .html(lsValue.message)
@@ -72,7 +78,7 @@ function initializeChat() {
                 lsTimestamp
                     .html(lsValue.timestamp)
                     .addClass("computer-timestamp")
-                setTimeout(function() { //Valorar eliminar timeout
+                setTimeout(function() { 
                     $("#end-of-messages-wrapper").before($(lsMessage));
                     $("#end-of-messages-wrapper").before($(lsTimestamp));
                 }, 1000)
@@ -83,22 +89,25 @@ function initializeChat() {
 
     function displayWelcomeMsg() { 
         
+        let welcomePhrase = "Tell me, O Muse, of the man of many devices, who wandered full many ways after he had sacked the sacred citadel of Troy.";
         let welcomeMsg = $("<div>");
         let welcomeTimeMsg = $("<div>");
+        
         welcomeMsg
             .addClass("computer-message")    
-            .html("Tell me, O Muse, of the man of many devices, who wandered full many ways after he had sacked the sacred citadel of Troy.");
+            .html(welcomePhrase);
+
         welcomeTimeMsg
             .addClass("computer-timestamp") 
             .html(helperFunctions.getCurrentTime());
+
         $("#end-of-messages-wrapper").before($(welcomeMsg));
-        $("#end-of-messages-wrapper").before($(welcomeTimeMsg))
-        let date = new Date();
-        localStorage.setItem(date.getTime(),
-            JSON.stringify({class: "ls-computer", message: "Tell me, O Muse, of the man of many devices, who wandered full many ways after he had sacked the sacred citadel of Troy.", timestamp: `${helperFunctions.getCurrentTime()}`}))
+        $("#end-of-messages-wrapper").before($(welcomeTimeMsg));
+        
+        helperFunctions.setLocalStorage("ls-computer",welcomePhrase);
     };
 
-    function deleteChatHistory() {
+    function clearChatHistory() {
         $("#clear-chat").click(function() {
            localStorage.clear();
             location.reload();
@@ -106,17 +115,17 @@ function initializeChat() {
     };
 
     displaySessionDate();
-    deleteChatHistory();
+    clearChatHistory();
     
-    if(localStorage.length > 0) {
+    if(localStorageArr.length > 0) {
         displayChatHistory();
     } else {
         displayWelcomeMsg();
     }
 
-    setTimeout(scrollToBottom,1000);
+    setTimeout(helperFunctions.scrollToBottom,1000);
 
-}
+};
     
 function runChat() {
     
@@ -129,9 +138,12 @@ function runChat() {
 
             function getUserInput(element) {
                 let $userInput = $(element).find("[id=user-input]");
-                function escapeHtml(str) { //Escapes tags to avoid XSS. Attribution: https://www.codegrepper.com/code-examples/javascript/jquery+escape+html+string
+                
+                //Escapes tags to avoid XSS. Attribution: https://www.codegrepper.com/code-examples/javascript/jquery+escape+html+string
+                function escapeHtml(str) { 
                     return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
                 };
+                
                 let userInput = escapeHtml($userInput.val()).trim();
                 return userInput;
             }
@@ -140,14 +152,15 @@ function runChat() {
                 let newUserMsg = $("<div>")
                     .addClass("user-message")
                     .html(getUserInput(element));
+
                 let newUserTime= $("<div>")
                     .addClass("user-timestamp")
                     .html(helperFunctions.getCurrentTime());
+
                 $("#end-of-messages-wrapper").before($(newUserMsg));
                 $("#end-of-messages-wrapper").before($(newUserTime));
-                let date = new Date();
-                localStorage.setItem(date.getTime(), //Sacar de esta función y dejar solo la llamada
-                    JSON.stringify({class: "ls-user", message: `${getUserInput(element)}`, timestamp: `${helperFunctions.getCurrentTime()}`}))
+                
+                helperFunctions.setLocalStorage("ls-user",`${getUserInput(element)}`);
             };
 
             // Resets form input on submit
@@ -157,7 +170,8 @@ function runChat() {
             
             function displayComputerMsg() {
 
-                function generateRandomSentence () { //Attribution: https://stackoverflow.com/questions/33160766/generate-random-sentences-from-an-array-javascript
+                //Attribution: https://stackoverflow.com/questions/33160766/generate-random-sentences-from-an-array-javascript
+                function generateRandomSentence () { 
                     let sentences= [
                         'So, surrender to sleep at last. What a misery, keeping watch through the night, wide awake -- you’ll soon come up from under all your troubles.',
                         'Nobody -- that’s my name. Nobody -- so my mother and father call me, all my friends.',
@@ -174,28 +188,31 @@ function runChat() {
                     let index = Math.floor(Math.random() * (maxSentences - 1));
                     return sentences[index];
                 }
+
                 let computerRandomText = generateRandomSentence();
+
                 let newComputerMsg = $("<div>")
                     .addClass("computer-message")
                     .html(computerRandomText);
+
                 let newComputerTime= $("<div>")
                     .addClass("computer-timestamp")
-                    .html(helperFunctions.getCurrentTime()); 
+                    .html(helperFunctions.getCurrentTime());
+
                 $("#end-of-messages-wrapper").before($(newComputerMsg));
                 $("#end-of-messages-wrapper").before($(newComputerTime));
-                let date = new Date();
-                localStorage.setItem(date.getTime(), //Sacar de esta función y dejar solo la llamada
-                    JSON.stringify({class: "ls-computer", message: `${computerRandomText}`, timestamp: `${helperFunctions.getCurrentTime()}`}))
+                
+                helperFunctions.setLocalStorage("ls-computer",`${computerRandomText}`);
             
             };
             
-            
+            // Validates that the form input is not blank or spaces and calls the chat functions (computer messages delayed for extra realism)
             if (getUserInput(this) !== "") {                
                 displayUserMsg(this);
                 resetForm(this);
-                scrollToBottom();
+                helperFunctions.scrollToBottom();
                 setTimeout(displayComputerMsg,1000);
-                setTimeout(scrollToBottom,1000);
+                setTimeout(helperFunctions.scrollToBottom,1000);
             };
                 
         }
